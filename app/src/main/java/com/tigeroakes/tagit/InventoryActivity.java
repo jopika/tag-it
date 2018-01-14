8ad1dbee1837d59f4dd667d7da4cb2eb80e7e4f5
Wenny Jiang <wwj9527@gmail.com>
2018-01-14 03:46:57 -0800

    Update InventoryActivity.java

--- app/src/main/java/com/tigeroakes/tagit/InventoryActivity.java
+++ app/src/main/java/com/tigeroakes/tagit/InventoryActivity.java
@@ -6,23 +6,23 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class InventoryActivity extends AppCompatActivity {
-    private SharedPreferences pref;
-    private TextView mNFCTag;
-    private TextView mNameTag;
+//     private SharedPreferences pref;
+//     private TextView mNFCTag;
+//     private TextView mNameTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
-        pref = getApplicationContext().getSharedPreferences("MyPref", 0);
-        String personalNFC = pref.getString( "personalNFC", "");
-        String name = pref.getString( "name", "");
+//         pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+//         String personalNFC = pref.getString( "personalNFC", "");
+//         String name = pref.getString( "name", "");
 
-        mNFCTag = (TextView) findViewById(R.id.nfc_code);
-        mNameTag = (TextView) findViewById(R.id.name_field);
+//         mNFCTag = (TextView) findViewById(R.id.nfc_code);
+//         mNameTag = (TextView) findViewById(R.id.name_field);
 
-        mNFCTag.setText(personalNFC);
-        mNameTag.setText(name);
+//         mNFCTag.setText(personalNFC);
+//         mNameTag.setText(name);
 
 
     }
